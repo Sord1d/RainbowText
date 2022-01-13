e46f289703f175662bcc08d32640f5c1c5387fb9
sord1d <andyahl95@sordiddev.eu>
2022-01-13 23:04:32 +0100

    Fixed chat output

--- src/main/java/eu/sordiddev/rainbowtext/commands/GradientAlogythm.java
+++ src/main/java/eu/sordiddev/rainbowtext/commands/GradientAlogythm.java
@@ -25,7 +25,7 @@ public class GradientAlogythm implements CommandExecutor {
     @Override
     public boolean onCommand ( CommandSender sender,  Command command,  String label,  String[] text) {
 
-        if (text.length == 0) {
+        if ( text.length == 0 ) {
             sender.sendMessage(ChatColor.GREEN + "");
             sender.sendMessage(ChatColor.GREEN + "RainbowText Help");
             sender.sendMessage(ChatColor.GREEN + "");
@@ -38,6 +38,7 @@ public class GradientAlogythm implements CommandExecutor {
             return true;
 
         }
+
         int currentposition = 1;
         List<String> colors = Arrays.asList("#ff0000", "#ff1200", "#ff2400", "#ff3700", "#ff4900", "#ff5b00", "#ff6d00", "#ff8000", "#ff9200", "#ffa400", "#ffb600", "#ffc800", "#ffdb00", "#ffed00", "#ffff00", "#f5ff00", "#ecff00", "#e2ff00", "#d8ff00", "#ceff00", "#c5ff00", "#bbff00", "#b1ff00", "#a8ff00", "#9eff00", "#94ff00", "#8aff00", "#81ff00", "#77ff00", "#6fff12", "#66ff24", "#5eff37", "#55ff49", "#4dff5b", "#44ff6d", "#3cff80", "#33ff92", "#2bffa4", "#22ffb6", "#1affc8", "#11ffdb", "#09ffed", "#00ffff", "#00f5ff", "#00ecff", "#00e2ff", "#00d8ff", "#00ceff", "#00c5ff", "#00bbff", "#00b1ff", "#00a8ff", "#009eff", "#0094ff", "#008aff", "#0081ff", "#0077ff", "#126fed", "#2466db", "#375ec8", "#4955b6", "#5b4da4", "#6d4492", "#803c80", "#92336d", "#a42b5b", "#b62249", "#c81a37", "#db1124", "#ed0912", "#ff0000");
         String inputtext = String.join(" ",text);
@@ -46,8 +47,7 @@ public class GradientAlogythm implements CommandExecutor {
         currentposition = 0;
         char currentletter;
 
-
-        if (textlength != 0) {
+        if ( textlength != 0 ) {
             while (textlength > currentposition) {
                 String hex = colors.get(color);
                 currentletter = inputtext.charAt(currentposition);
@@ -58,7 +58,7 @@ public class GradientAlogythm implements CommandExecutor {
                 }
                 currentposition = currentposition +1;
 
-                if (color < 70) {
+                if ( color < 70 ) {
                     color = color + 1;
                 } else {
                     color = 1;
