16bbc4e05c6007bcf6cd96fecf23e217436f7b64
sord1d <andyahl95@sordiddev.eu>
2022-01-16 13:16:23 +0100

    Preparing for dynamic text generation

--- src/main/java/eu/sordiddev/rainbowtext/RainbowText.java
+++ src/main/java/eu/sordiddev/rainbowtext/RainbowText.java
@@ -1,6 +1,7 @@
 package eu.sordiddev.rainbowtext;
 
 import eu.sordiddev.rainbowtext.commands.*;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RainbowText extends JavaPlugin {
@@ -8,11 +9,21 @@ public final class RainbowText extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        // old commands
+
         int color;
         getCommand("rtrs").setExecutor(new AlgorythmStatic(1));
         getCommand("rtgs").setExecutor(new AlgorythmStatic(30));
         getCommand("rtbs").setExecutor(new AlgorythmStatic(45));
         getCommand("rtys").setExecutor(new AlgorythmStatic(15));
+
+        //Commands for the new generation method
+
+        getCommand("rtr").setExecutor(new AlgorythmDynamic(1));
+        getCommand("rtg").setExecutor(new AlgorythmDynamic(2));
+        getCommand("rtb").setExecutor(new AlgorythmDynamic(3));
+        getCommand("rty").setExecutor(new AlgorythmDynamic(4));
+        getCommand("rt").setExecutor(new AlgorythmDynamic(5));
     }
 
     @Override
--- src/main/java/eu/sordiddev/rainbowtext/commands/AlgorythmDynamic.java
+++ src/main/java/eu/sordiddev/rainbowtext/commands/AlgorythmDynamic.java
@@ -1,5 +1,35 @@
 package eu.sordiddev.rainbowtext.commands;
 
-public class AlgorythmDynamic {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class AlgorythmDynamic implements CommandExecutor {
+
+    //get the color variable. 1 -> red; 2-> green; 3 -> blue; 4 -> yellow; 5 -> rainbow
+
+    int color = 0;
+    public AlgorythmDynamic(int i) {
+        color = i;
+    }
+    /*
+    color codes
+    RED         ff0000  111111110000000000000000
+    GREEN       6fff12  011011111111111100010010
+    BLUE        00f5ff  000000001111010111111111
+    YELLOW      ffff00  111111111111111100000000
+     */
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] text) {
+
+        //getting the text length and the input text
+        String inputtext = String.join(" ",text);
+        String outputtext = "";
+        int textlength = inputtext.length();
+
+        return false;
+    }
     //todo
 }
