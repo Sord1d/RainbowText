280d7e21f7a38fec1515ede00fd07ef68a8bba50
sord1d <andyahl95@sordiddev.eu>
2022-01-26 14:15:26 +0100

    Preparing for dynamic text generation

--- src/main/java/eu/sordiddev/rainbowtext/commands/AlgorythmDynamic.java
+++ src/main/java/eu/sordiddev/rainbowtext/commands/AlgorythmDynamic.java
@@ -1,15 +1,27 @@
 package eu.sordiddev.rainbowtext.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.awt.image.ColorModel;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 
 public class AlgorythmDynamic implements CommandExecutor {
 
     //get the color variable. 1 -> red; 2-> green; 3 -> blue; 4 -> yellow; 5 -> rainbow
-
-    int color = 0;
+    List<String> colors = new ArrayList<>();
+    int color;
+    Color loopcolor;
     public AlgorythmDynamic(int i) {
         color = i;
     }
@@ -29,7 +41,63 @@ public class AlgorythmDynamic implements CommandExecutor {
         String outputtext = "";
         int textlength = inputtext.length();
 
+
+       //Defining start values for the colors depending on the command TODO
+
+
+        float hue1 = Color.RGBtoHSB(10
+                , 1, 10, null)[0];
+        float hue2 = Color.RGBtoHSB(100, 100, 110, null)[0];
+
+        //defining text length n and starting a loop for n steps
+
+        int n = textlength;
+        for (int i = 0; i < n; i++) {
+
+            //calculating a gradient due to the power of maths - yay maths (I googled the maths formula)
+           loopcolor =  Color.getHSBColor(hue2 + (i * (hue2 - hue1) / n), 1, 1);
+
+           //getting rgb values and converting it to hex, adding the color to the color list
+            int red = loopcolor.getRed();
+            int green = loopcolor.getGreen();
+            int blue = loopcolor.getBlue();
+            String hex = String.format("#%02x%02x%02x", red, green, blue);
+            colors.add(hex);
+
+        }
+
+        sender.sendMessage(String.valueOf(colors));
+
         return false;
     }
-    //todo
+
+    /*This class is based on code from stackoverflow.
+    public class RBCalc<Queue> {
+        public int n;
+
+        //get the text length
+        public RBCalc(int textlength) {
+            int n = textlength;
+        }
+
+        float hue1 = Color.RGBtoHSB(0
+                , 1, 0, null)[0];
+        float hue2 = Color.RGBtoHSB(0, 0, 1, null)[0];
+
+
+
+        void colorlist() {
+            for (int i = 0; i < n; i++) {
+
+                colors.add(String.valueOf(Color.getHSBColor(hue2 + (i * (hue2 - hue1) / n), 1, 1)));
+            }
+
+            return;
+
+        }
+
+    }*/
+
+
+
 }
